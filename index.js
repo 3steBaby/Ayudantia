71441640773566287e16047f1104170f4b3ad825
Esteban <stbnrivasa@gmail.com>
2023-08-31 23:40:07 -0400

    index changes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require ('express'); //
-// const mongoose = require ('mongoose'); //Bdd
-// const cors = require ('cors'); //Seguridad
-// require('dotenv').config();
+const mongoose = require ('mongoose'); //Bdd
+const cors = require ('cors'); //Seguridad
+require('dotenv').config();
 
 const app = express();
 
-app.listen(3001, () => Console.log('Server started'));
+app.listen(process.env.PORT, () => {
+    for (let i = 0; i < 10 ; i++) {
+        console.log('Server started for ' + i);
+    }
+});
