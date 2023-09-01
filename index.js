2e59230d3f0fcd90d0fff334179d4cd5cddd4ce4
Esteban <stbnrivasa@gmail.com>
2023-08-31 22:57:31 -0400

    Actualizacion

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express'); //
-const mongoose = require ('mongoose'); //Bdd
-const cors = require ('cors'); //Seguridad
-require('dotenv').config();
+// const mongoose = require ('mongoose'); //Bdd
+// const cors = require ('cors'); //Seguridad
+// require('dotenv').config();
 
 const app = express();
 
